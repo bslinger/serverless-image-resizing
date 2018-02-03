eaf4b26e4c0aa28c2267607a99b9f6101ff88385
Ben Slinger <ben@directshop.com.au>
2018-02-03 12:09:32 +1100

    Modify to retrieve remote image

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+var https = require('https');
+var request = require('request').defaults({ encoding: null });
+var base64 = require('base-64');
+
 const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
@@ -12,44 +16,89 @@ const ALLOWED_DIMENSIONS = new Set();
 
 if (process.env.ALLOWED_DIMENSIONS) {
   const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
-  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
+  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension)
+)
+  ;
 }
 
-exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
+const getFallbackImage(url) = (image) => {
+  request.get(url, function (error, response, body))
+  {
+
+  }
+};
+
+exports.handler = function (event, context, callback) {
+
+  console.log(event, context, callback);
+  const path = event.path;
+  const pathBits = path.split(/\/+/);
+  console.log(pathBits);
+  const match = pathBits[0].match(/((\d+)x(\d+))/);
+  if (!match) {
+    callback(null, {
+      statusCode: '403',
+      headers: {},
+      body: ''
+    });
+    return;
+  }
   const dimensions = match[1];
   const width = parseInt(match[2], 10);
   const height = parseInt(match[3], 10);
-  const originalKey = match[4];
 
-  if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
-     callback(null, {
+  if (pathBits.length <= 1)
+  {
+    callback(null, {
       statusCode: '403',
       headers: {},
-      body: '',
+      body: ''
     });
     return;
   }
 
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
+  const encodedURL = pathBits[1];
+
+  if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
+    callback(null, {
+      statusCode: '403',
+      headers: {},
+      body: ''
+    });
+    return;
+  }
+
+  let replaceUrl = null;
+  if (pathBits.length > 1)
+  {
+    replaceUrl = base64.decode(pathBits[2]);
+  }
+
+  const imageURL = base64.decode(encodedURL);
+
+  console.log("Getting URL", imageURL);
+
+  request.get(imageURL, function (error, response, body) {
+      if (response.statusCode !== 200)
+      {
+
+      }
+      Sharp(Buffer.from(body))
+        .resize(width, height)
+        .toFormat('png')
+        .toBuffer()
+        .then(buffer => S3.putObject({
         Body: buffer,
         Bucket: BUCKET,
         ContentType: 'image/png',
-        Key: key,
-      }).promise()
-    )
+        Key: path,
+      }).promise())
     .then(() => callback(null, {
         statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
+        headers: { 'location': `${URL}/${path}` },
         body: '',
-      })
-    )
-    .catch(err => callback(err))
+      }))
+    .catch(err => callback(err));
+
+    });
 }
---
+++ lambda/test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import {handler} from './';
+
+handler({path:'30x30/aHR0cHM6Ly9jb20tcG9kaWFudC5hbXMzLmRpZ2l0YWxvY2VhbnNwYWNlcy5jb20vbWVkaWEvc3Bva2UvYml0c3Rvcm0vYXJ0d29yay8zNTVjYTZhNGRiZjNkMC5qcGc='}, 'context', (args) => console.log(args));
