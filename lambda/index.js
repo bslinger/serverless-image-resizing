5b8f904aad38ebdf0dfb4498e251ddba270c3cc5
Ben Slinger <ben@directshop.com.au>
2018-02-03 21:11:01 +1100

    First pass at final code

--- lambda/index.js
+++ lambda/index.js
@@ -3,32 +3,43 @@
 const AWS = require('aws-sdk');
 var https = require('https');
 var request = require('request').defaults({ encoding: null });
+var rp = require('request-promise-native');
 var base64 = require('base-64');
+var utf8 = require('utf8');
 
 const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 const Sharp = require('sharp');
 
-const BUCKET = process.env.BUCKET;
-const URL = process.env.URL;
-const ALLOWED_DIMENSIONS = new Set();
+function String2Hex(tmp) {
+  var str = '';
+  for(var i = 0; i < tmp.length; i++) {
+    str += tmp[i].charCodeAt(0).toString(16);
+  }
+  return str;
+}
 
 if (process.env.ALLOWED_DIMENSIONS) {
   const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
-  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension)
-)
-  ;
+  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
 }
 
-const getFallbackImage(url) = (image) => {
-  request.get(url, function (error, response, body))
-  {
+const doResize = (body, width, height) => {
 
-  }
-};
+  let buffer = Buffer.from(body);
+  console.log(buffer);
+  return Sharp(buffer)
+    .resize(width, height)
+    .toFormat('png')
+    .toBuffer();
+}
 
 exports.handler = function (event, context, callback) {
+
+  const BUCKET = process.env.BUCKET;
+  const URL = process.env.URL;
+  const ALLOWED_DIMENSIONS = new Set();
 
   console.log(event, context, callback);
   const path = event.path;
@@ -47,8 +58,7 @@ exports.handler = function (event, context, callback) {
   const width = parseInt(match[2], 10);
   const height = parseInt(match[3], 10);
 
-  if (pathBits.length <= 1)
-  {
+  if (pathBits.length <= 1) {
     callback(null, {
       statusCode: '403',
       headers: {},
@@ -69,36 +79,60 @@ exports.handler = function (event, context, callback) {
   }
 
   let replaceUrl = null;
-  if (pathBits.length > 1)
-  {
-    replaceUrl = base64.decode(pathBits[2]);
+  if (pathBits.length > 1) {
+    try {
+      replaceUrl = base64.decode(pathBits[2]);
+    }
+    catch (e) {
+      // just don't use a replace URL?
+    }
   }
 
-  const imageURL = base64.decode(encodedURL);
+  let imageURL = null;
+  try {
+    imageURL = base64.decode(encodedURL);
+  }
+  catch (e) {
+    return callback(null, {
+      statusCode: '404',
+      body: 'error decoding image URL'
+    });
+  }
 
   console.log("Getting URL", imageURL);
 
-  request.get(imageURL, function (error, response, body) {
-      if (response.statusCode !== 200)
-      {
-
-      }
-      Sharp(Buffer.from(body))
-        .resize(width, height)
-        .toFormat('png')
-        .toBuffer()
+  rp.get({uri: imageURL, resolveWithFullResponse: true, encoding: null})
+    .then((response) => {
+    //console.log(response);
+      return doResize(response.body, width, height)
         .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/png',
-        Key: path,
-      }).promise())
+          Body: buffer,
+          Bucket: BUCKET,
+          ContentType: 'image/png',
+          Key: path,
+        }).promise());
+    })
     .then(() => callback(null, {
-        statusCode: '301',
-        headers: { 'location': `${URL}/${path}` },
-        body: '',
-      }))
-    .catch(err => callback(err));
-
+      statusCode: '301',
+      headers: { 'location': `${URL}/${path}` },
+      body: '',
+    }))
+    .catch((e) => {
+      if (e.response)
+      {
+        console.log("failed", e.response.statusCode, replaceUrl);
+      }
+      else
+      {
+        console.log("failed", e.message);
+      }
+      return callback(null, replaceUrl ? {
+          statusCode: '302',
+          headers: { location: replaceUrl },
+          body: ''
+        } : {
+          statusCode: '404'
+        })
     });
+
 }
--- lambda/test.js
+++ lambda/test.js
@@ -1,5 +1,15 @@
 'use strict';
 
+require('dotenv').config();
+
+import {encode} from 'base-64';
+
 import {handler} from './';
 
-handler({path:'30x30/aHR0cHM6Ly9jb20tcG9kaWFudC5hbXMzLmRpZ2l0YWxvY2VhbnNwYWNlcy5jb20vbWVkaWEvc3Bva2UvYml0c3Rvcm0vYXJ0d29yay8zNTVjYTZhNGRiZjNkMC5qcGc='}, 'context', (args) => console.log(args));
+let validPath = encode('https://www.google.com.au/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png');
+//let validPath = 'aHR0cHM6Ly9jb20tcG9kaWFudC5hbXMzLmRpZ2l0YWxvY2VhbnNwYWNlcy5jb20vbWVkaWEvc3Bva2UvYml0c3Rvcm0vYXJ0d29yay8zNTVjYTZhNGRiZjNkMC5qcGc=';
+let invalidPath = 'aHR0cHM6Ly9jb20tcG9kaWFudC5hbXMzLmRpZ2l0YWxvY2VhbnNwYWNlcy5jb20vbWVkaWEvc3Bva2UvYml0c3Rvcm0vYXJ0d29yay8zNTVjYTZhNGRiZjNkLmpwZw==';
+let fallback = 'aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc=';
+
+handler({path: `30x30/${validPath}` }, 'context', (args) => console.log(args));
+//handler({path: `30x30/${invalidPath}/${fallback}` }, 'context', (a, b) => console.log(b));
